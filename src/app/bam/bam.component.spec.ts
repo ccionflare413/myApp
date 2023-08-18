58bbeaacdb8d0b400b0179a547a262a8fd1447ec
RapatNG <Rapat.Ng2543@gmail.com>
2023-08-18 17:04:08 +0700

    Bam Work

---
+++ src/app/bam/bam.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { BamComponent } from './bam.component';
+
+describe('BamComponent', () => {
+  let component: BamComponent;
+  let fixture: ComponentFixture<BamComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [BamComponent]
+    });
+    fixture = TestBed.createComponent(BamComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
